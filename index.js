cf5625ffa3a72c1f8819506914adf4422f738d40
arnaud ferrand <arferrand@gmail.com>
2018-10-07 21:14:16 +0100

    add comments on export function

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 const opencage = require('opencage-api-client');
 
+/**
+ * geocode using opencage geocoder API
+ *
+ * @param  {Object} request  http request, same as opencage API [see](https://opencagedata.com/api), exception API key
+ * @param  {Object} response opencage geocoder API response
+ */
 exports.geocode = (request, response) => {
   if (!request.query) {
     response.status(400).send({error: 400, message: "Couldn't read query parameters"});
     return;
   }
 
-  if (typeof process.env.OCD_API_KEY === 'undefined') {
+  if (typeof process.env.OCD_API_KEY === 'undefined' && typeof request.query.key === 'undefined') {
     response.status(403).send({error: 403, message: 'missing API key'});
     return;
   }
