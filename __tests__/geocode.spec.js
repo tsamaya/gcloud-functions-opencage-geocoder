e3f3dafeb832619344a3e1723de4f59bcb58ee81
arnaud ferrand <arferrand@gmail.com>
2018-10-12 01:26:36 +0100

    adding unit tests

---
+++ __tests__/geocode.spec.js
@@ -0,0 +1,124 @@
+const opencage = require('../');
+
+describe('OpenCage Lib suite', () => {
+  test('library exists', () => {
+    expect(opencage).toBeTruthy();
+    expect(typeof opencage).toBe('object');
+  });
+
+  beforeAll(() => {
+    jest.mock('opencage-api-client');
+  });
+  afterAll(() => {
+    jest.unmock('opencage-api-client');
+  });
+
+  let response;
+
+  beforeEach(() => {
+    response = {
+      statusCode: 0,
+      body: {},
+      status: jest.fn(code => {
+        response.statusCode = code;
+        return {
+          send: jest.fn(body => {
+            response.body = body;
+            return true;
+          }),
+        };
+      }),
+    };
+  });
+
+  describe('Rainy Tests', () => {
+    describe('#Query String', () => {
+      test('no query parameters', async done => {
+        const request = {};
+
+        try {
+          await opencage.geocode(request, response);
+          expect(response.body).toEqual(opencage.ERROR_QUERY_PARAMETERS);
+        } catch (err) {
+          expect(false).toBeTruthy();
+        } finally {
+          done();
+        }
+      });
+
+      test('no queryStringParameters', async done => {
+        const request = {
+          query: {},
+        };
+
+        try {
+          await opencage.geocode(request, response);
+          expect(response.body).toEqual(opencage.ERROR_QUERY_PARAMETERS);
+        } catch (err) {
+          expect(false).toBeTruthy();
+        } finally {
+          done();
+        }
+      });
+
+      test('rejection', async done => {
+        const request = {
+          query: { q: 'networkerror' },
+        };
+
+        try {
+          await opencage.geocode(request, response);
+          expect(response.body).toBeTruthy();
+        } catch (err) {
+          expect(false).toBeTruthy();
+        } finally {
+          done();
+        }
+      });
+    });
+
+    describe('#Environment', () => {
+      let backup;
+
+      beforeAll(() => {
+        backup = process.env.OCD_API_KEY;
+        delete process.env.OCD_API_KEY;
+      });
+
+      afterAll(() => {
+        process.env.OCD_API_KEY = backup;
+      });
+
+      test('no env var', async done => {
+        const request = {
+          query: { q: 'Brandenburg Gate' },
+        };
+
+        try {
+          await opencage.geocode(request, response);
+          expect(response.body).toEqual(opencage.MISSING_API_KEY);
+        } catch (err) {
+          expect(false).toBeTruthy();
+        } finally {
+          done();
+        }
+      });
+    });
+  });
+  describe('Mocked Tests', () => {
+    test('reverse geocode `Brandenburg Gate`', async done => {
+      const request = {
+        query: { q: 'Brandenburg Gate' },
+      };
+
+      try {
+        await opencage.geocode(request, response);
+        expect(response.body).toBeTruthy();
+      } catch (err) {
+        expect(false).toBeTruthy();
+      } finally {
+        done();
+      }
+    });
+  });
+});
